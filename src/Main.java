e5c9abaac206b146d98176faa62fe95ceac6d49b
norki <norkink89@mail.ru>
2023-02-03 17:44:33 +0400

    completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import transport.Car;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car car5 = new Car("Hyundai", "Avant", 1.6, "оранжевый", "Korea",2016, "auto", "cabriolet", 13245, 4,true);
+        System.out.println(car5); // распечатка созданного объекта
+        car5.changeTyre(11); //функционал по смене резины
+               System.out.println(car5); //распечатка объекта после смены резины
     }
 }
